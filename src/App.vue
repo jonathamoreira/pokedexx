ce544a1a3e080a591afd562a0987367d9750a39c
Jonatha <johnninajoao@gmail.com>
2023-08-09 17:21:40 -0300

    :up:

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <div v-for="(poke, index) in pokemons" :key="index">
-      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    <div class="column is-half is-offset-one-quarter ">
+      <img src="./assets/pokedex.png" alt="logo">
+      <hr>
+      <input type="text" placeholder="buscar pokemon" v-model="busca" class="input is-rounded">
+      <button class="button is-success is-focused" id="buscabtn" @click="buscar">Buscar</button>
+      <div v-for="(poke, index) in filterdPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
     </div>
   </div>
 </template>
@@ -14,17 +20,40 @@ export default {
   name: 'App',
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      filterdPokemons: [],
+      busca:''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
       this.pokemons = res.data.results;
+      this.filterdPokemons = res.data.results;
     })
   },
   components: {
     Pokemon
+  },
+  methods: {
+    buscar: function(){
+      this.filterdPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == " "){
+        this.filterdPokemons = this.pokemons;
+      }else{
+        this.filterdPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
   }
+  /*
+  computed: { //Resultado em tempo real, não precisa do buttun
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == " "){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+  }*/
 }
 </script>
 
@@ -36,5 +65,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#buscabtn{
+  margin-top: 2%;
 }
 </style>
